dac2d60ccf63b10d428002d3b5be97a1b73cb048
Kevin Carrogan <kevin@carrogan.com>
2025-01-07 22:01:55 +0000

    Add mouse interactions

--- src/index.js
+++ src/index.js
@@ -1,36 +1,41 @@
-import Matter from "matter-js";
+import {
+  Engine,
+  Render,
+  Runner,
+  Bodies,
+  Composite,
+  Mouse,
+  MouseConstraint,
+} from "matter-js";
 
 const container = document.querySelector("#container");
 
-// module aliases
-var Engine = Matter.Engine,
-  Render = Matter.Render,
-  Runner = Matter.Runner,
-  Bodies = Matter.Bodies,
-  Composite = Matter.Composite;
-
-// create an engine
-var engine = Engine.create();
-
-// create a renderer
-var render = Render.create({
+const engine = Engine.create();
+const render = Render.create({
   element: container,
   engine: engine,
 });
 
-// create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+const boxA = Bodies.rectangle(400, 200, 80, 80);
+const boxB = Bodies.rectangle(450, 50, 80, 80);
+const ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
-// add all of the bodies to the world
+// add mouse control
+const mouse = Mouse.create(render.canvas),
+  mouseConstraint = MouseConstraint.create(engine, {
+    mouse: mouse,
+    constraint: {
+      stiffness: 0.2,
+      render: {
+        visible: false,
+      },
+    },
+  });
+
+Composite.add(engine.world, mouseConstraint);
 Composite.add(engine.world, [boxA, boxB, ground]);
 
-// run the renderer
 Render.run(render);
 
-// create runner
-var runner = Runner.create();
-
-// run the engine
+const runner = Runner.create();
 Runner.run(runner, engine);
